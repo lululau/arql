a50436a0f765bf046e433a24803c282a21283947
Liu Xiang <liuxiang@ktjr.com>
2020-08-27 22:30:20 +0800

    Fix bug

--- lib/arql/repl.rb
+++ lib/arql/repl.rb
@@ -7,7 +7,7 @@ module Arql
   class Repl
     def initialize
       Pry.config.prompt = Pry::Prompt.new("", "", prompt)
-      main_object.pry
+      Pry.start
     end
 
     def main_object
@@ -29,7 +29,11 @@ module Arql
          if obj == main_object && nest_level == 0
            nest_level_prompt = ''
          else
-           nest_level_prompt = "(#{obj}:#{nest_level})"
+           nest_level_prompt = if nest_level.zero?
+                                 "(#{obj})"
+                               else
+                                 "(#{obj}:#{nest_level})"
+                               end
          end
          "%s#{Rainbow('@').green}%s#{nest_level_prompt} %s " % [Rainbow('ARQL').red, Rainbow(App.prompt).yellow, Rainbow('❯').green]
        end]
