586e50c47cfbdd0ea62e8315162590ee763f5aeb
Liu Xiang <liuxiang@ktjr.com>
2022-08-22 16:18:17 +0800

    Add helper methods to ActiveRecord::Result

--- lib/arql/definition.rb
+++ lib/arql/definition.rb
@@ -284,5 +284,31 @@ module Arql
         records.dump(filename, batch_size)
       end
     end
+
+    ::ActiveRecord::Result.class_eval do
+      def t(*attrs, **options)
+        to_a.t(*attrs, **options)
+      end
+
+      def vd(*attrs, **options)
+        to_a.vd(*attrs, **options)
+      end
+
+      def v
+        to_a.v
+      end
+
+      def a
+        to_a
+      end
+
+      def write_csv(filename, *fields, **options)
+        to_a.write_csv(filename, *fields, **options)
+      end
+
+      def write_excel(filename, *fields, **options)
+        to_a.write_excel(filename, *fields, **options)
+      end
+    end
   end
 end
