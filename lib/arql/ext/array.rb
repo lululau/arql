916d946e1b76f2dd5bb0bb8ca67ad9a0ab69e614
Liu Xiang <liuxiang@ktjr.com>
2023-11-15 16:13:34 +0800

    Fix typo

--- lib/arql/ext/array.rb
+++ lib/arql/ext/array.rb
@@ -184,7 +184,7 @@ class Array
       params = Struct.new(:title, :width, :height, :border, :margin, :padding, :color, :xlabel,
           :ylabel, :labels, :symbol, :xscale, :nbins, :closed, :canvas, :xlim, :ylim, :grid, :name) do
         def to_hc
-          {}
+          to_h.compact
         end
       end.new
       fmt = 'xyy'
