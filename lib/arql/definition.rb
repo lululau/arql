1b008e4b7ab803b41b2f258c4dd6d5f17bb9ad9c
Liu Xiang <liuxiang@ktjr.com>
2023-12-19 15:01:44 +0800

    Add new methods to Arql and Ransack::Search classes

--- lib/arql/definition.rb
+++ lib/arql/definition.rb
@@ -319,6 +319,40 @@ module Arql
       def write_excel(filename, *fields, **options)
         to_a.write_excel(filename, *fields, **options)
       end
+
+      def dump(filename, batch_size=500)
+        to_a.dump(filename, batch_size)
+      end
+    end
+
+    ::Ransack::Search.class_eval do
+      def t(*attrs, **options)
+        result.t(*attrs, **options)
+      end
+
+      def vd(*attrs, **options)
+        result.vd(*attrs, **options)
+      end
+
+      def v
+        result.v
+      end
+
+      def a
+        result.a
+      end
+
+      def write_csv(filename, *fields, **options)
+        result.write_csv(filename, *fields, **options)
+      end
+
+      def write_excel(filename, *fields, **options)
+        result.write_excel(filename, *fields, **options)
+      end
+
+      def dump(filename, batch_size=500)
+        result.dump(filename, batch_size)
+      end
     end
   end
 end
